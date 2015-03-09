86a674f6d3b3abd99eeca5b6a7a5f70a41374269
LingboTang <lingbo19.tang@gmail.com>
2015-03-09 14:32:53 -0600

    asdfkasdjf

--- Assignment2/server.c
+++ Assignment2/server.c
@@ -1,52 +1,61 @@
-#include <sys/socket.h>
-#include <sys/un.h>
+/* This server receives an integer value encoded as a string from a client through UDP socket, and sends back the factorial of that number to the client  */
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
-/* --------------------------------------------------------------------- */
-/* This is a sample server which opens a stream socket and then awaits   */
-/* requests coming from client processes. In response for a request, the */
-/* server sends an integer number such that different processes get      */
-/* distinct numbers. We assume that the server and the clients run on    */
-/* the same machine.                                                     */
-/* --------------------------------------------------------------------- */
+#define BUFLEN 512
+#define PORT 9930
+#define IP 2130706433  /* 127.0.0.1 */
 
-int main(void) {
-    int sock, snew, fromlength, number;
-    struct sockaddr_un master, from;
-
-    sock = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror ("Server: cannot open master socket");
-        exit (1);
-    }
-
-    strcpy(master.sun_path, "the_number_server");
-    master.sun_family = AF_UNIX;
-
-
-	//GET A TELEPHONE NUMBER TO THE PHONE
-    if (bind(sock, (struct sockaddr*) &master, sizeof(struct sockaddr_un))) {
-        perror ("Server: cannot bind master socket");
-        exit (1);
-    }
-
-    number = 0;
-	//DROP IT WHEN GETTING 6 MORE WAIT
-    listen(sock, 5);
-
-    while (1) {
-        fromlength = sizeof(from);
-        snew = accept(sock, (struct sockaddr*) &from, &fromlength);
-        if (snew < 0) {
-            perror ("Server: accept failed");
-            exit (1);
-        }
-
-        write(snew, &number, sizeof(number));
-        close(snew);
-        number++;
-    }
-    return 0;
+int fact (int n)
+{
+	if (n <= 1)
+		return 1;
+	else
+		return n * fact (n-1);
 }
+
+int main(void)
+{
+	struct sockaddr_in si_me, si_other;
+	int s, i, slen=sizeof(si_other);
+	char buf[BUFLEN];
+
+	if ( ( s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP) ) == -1 )
+	{
+		printf("Error in creating socket");
+		return 1;
+	}
+
+	memset((char *) &si_me, 0, sizeof(si_me));
+	si_me.sin_family = AF_INET;
+	si_me.sin_port = htons(PORT);
+	si_me.sin_addr.s_addr = htonl(IP); /* htonl(INADDR_ANY) for any interface on this machine */
+
+	if ( bind(s, &si_me, sizeof(si_me)) == -1 )
+	{
+		printf("Error in binding the socket");
+		return 2;
+	}
+
+	printf("\n\nServer listening to %s:%d\n\n", inet_ntoa(si_me.sin_addr), ntohs(si_me.sin_port));
+	while (1) 
+	{
+		if ( recvfrom(s, buf, BUFLEN, 0, &si_other, &slen) != -1)
+		{
+			printf("\nReceived packet from %s:%d  Data: %s\n\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), buf);
+			i = atoi(buf);
+			sprintf(buf, "%d", fact(i));
+			printf("\nSending Fact(%d): %s to %s:%d\n", i, buf, inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+			sendto(s, buf, strlen(buf) + 1, 0, &si_other, sizeof(si_other));
+		}
+	}
+
+	close(s);
+ 	return 0;
+ }
