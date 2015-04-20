34c7ec7ae6f6cb4ba4b22278c758b74a6c50687b
Lingbo Tang <lingbo19.tang@gmail.com>
2015-04-20 05:48:39 -0600

    sdfkajsdfj

--- Final/Lock.c
+++ Final/Lock.c
@@ -103,7 +103,7 @@ void Peterson()
 			//critical section
 		flag[i] = 0;
 			//remainder section
-	}while (true)
+	}while (true);
 }
 
 /* Synchronization Hardware */
@@ -120,7 +120,8 @@ void locks()
 		//critical section
 	// release lock
 		//remainder section
-}while (true)
+	while (true);
+}
 
 /* Test and Set */
 int test_and_set (int *target)
@@ -142,5 +143,268 @@ void using_test_and_set(int lock)
 		lock = 0;
 
 		/* remainder section*/
-	}while(true)
+	}while(true);
 }
+
+// Compare_and_Swap Instruction
+int compare_and_swap(int *value, int expected, int new_value)
+{
+	int temp = *value;
+
+	if(*value == expected)
+	{
+		*value = new_value;
+	}
+	return temp;
+}
+
+void using_compare_and_swap(int *value,int expected,int new_value,int lock)
+{
+	do{
+		while(compare_and_swap(&lock,0,1) !=0)
+			;
+
+		/* critical section */
+
+		lock = 0;
+
+		/* remainder section */
+
+	}while(true);
+}
+
+// Bounded-waiting with test_and_set
+void bounded()
+{
+	do
+	{
+		waiting[i] = true;
+		key = true;
+		while(waiting[i] && key)
+			key = test_and_set(&lock);
+		wating[i] = 0;
+
+		/* critical section */
+		j = (i+1) % n;
+		while((j!=i) && !waiting[j])
+			j = (j+1) % n;
+		if (j == i)
+			lock = 0;
+		else
+			waiting[j] = 0;
+		/* remainder section */
+	}while(true);
+}
+
+// Mutex Locks
+// Some software based Solutions
+	// Simplest is mutex lock (mutual exclusion)
+// Protect a critical section by first acquire() a lock then release() the lock
+// Calls to acquire() and release() must be atomic
+	//Usually implemented via Hardware atomic instructions
+// But this Solution requires busy waiting
+	//This mutex lock is called spinlock
+
+
+void acquire()
+{
+	while (!available)
+		; /* busy waiting */
+	available = 0;
+}
+
+void release()
+{
+	available = 1;
+}
+
+void acquire_and release()
+{
+	do{
+		acquire()
+		/* critical section */
+		release()
+		/* remainder section */
+	}while(true);
+}
+
+// Semaphore
+// Sophisticated tool to synchronize process activities
+// Semaphore S - int variable
+// Can only be accessed via two atomic operations
+// wait() and signal()
+
+void wait(S){
+	while (S<=0)
+		; // busy wait
+	S --;
+}
+
+void signal(S)
+{
+	S++;
+}
+
+// Counting Semaphore - int value can range over an unrestricted domain
+// 		May be used to access a resource; initialized to # of instances
+// Binary Semaphore - int value that can be 0 or 1
+//	 	May be used for critical section problem; same as a mutex lock
+// Can solve various synchronization problems
+//		Consider P1 and P2 that require S1 to happen before S2
+//		Create a Semaphore "synch" initialized to 0
+
+// P1: ...
+//		S1;
+//		signal(synch);
+//	   ...
+// P2: ...
+//		wait(synch);
+//	   S2;
+//	   ...
+
+
+// Semaphore Implementation without Busy waiting
+// With each semaphore there is an associated waiting queue
+// Each semaphore is defined as
+typedef struct {
+	int value;
+	struct process * list;
+} semaphore;
+
+// Two additional operations (implemented as sys calls):
+//	block - place the process invoking the operation on the waiting queue of related semaphore
+//  wakeup - remove one of process from the waiting queue and place it in the ready queue
+
+void xwait(semaphore *S)
+{
+	S -> value--;
+	if (S-> value < 0) {
+		//add this process to S->list;
+		block();
+	}
+}
+
+void xsignal(semaphore *S)
+{
+	S->value++;
+	if (S->value <=0)
+	{
+		//remove a process P from S-> list;
+		wakeup(P);
+	}
+}
+
+// Deadlock and Starvation
+// Deadlock - two or more processes are waiting indefinitely for an
+// event that can be caused by only one of the waiting processes
+// Let S and Q be two semaphores initialized to 1
+//        P0           P1
+//       wait(S);	wait(Q);
+//       wait(Q);	wait(S);
+//       ...          ...
+//       signal(S); signal(Q);
+//       signal(Q); signal(S);
+// Starvation - indefinite blocking
+//	A process may never be removed from the semaphore queue(saym if
+//  we use LIFO scheme)
+
+// Dining-Philosophers problem
+
+void philosopher()
+{
+	do{
+		//think
+
+		wait(chopstick[i]);
+		wait(chopstick[(i+1)%5]);
+
+		//eat
+	}while(true);
+}
+
+// May create deadlock and Starvation
+
+/* Monitors */
+// Programming error in semaphore may result deadlock or Starvation
+// Monitor is a more robust high-level abstracttion for synchronization
+// that combine and hides
+
+//	shared data, operations on data, synchronization with condition variables
+
+struct monitor{
+	// condition & shared variable declarations
+	// function F1 (...) {...}
+	// function Fn (...) {...}
+	// Initialization_code (...) {...}
+};
+
+// Monitor's local variables can only be accessed by local functions
+// local fucntions can only access local variables and parameters
+// Only one process may be active within the Monitor at a time
+// ENsures mutual exclusion
+
+monitor sharedcounter()
+{
+	int counter
+	function add() {counter++;}
+	function sub() {counter--;}
+	Initialization_code() {counter=0;}
+}
+
+// If a process calls sharedcounter.add(), then it has exclusive access to the
+// Monitor until it leaves add()
+//	No other process can come in, and has to wait
+
+// condition x, y
+// Two operations are allowed on a condition variable:
+// x.wait() - a process that invokes the operation is suspended until another process invokes x.signal()
+// x.signal() - resumes one of processes(if any) that invokes x.wait()
+//				if no x.wait() on the variable, then it has no effect
+
+monitor DiningPhilosophers
+{
+	enum{ THINKING;HUNGRY,EATING} state [5];
+	condition self [5];
+
+	void pickup(int i)
+	{
+		state[i] = HUNGRY;
+		test(i);
+		if (state[i]!=EATING)
+			self[i].wait;
+	}
+
+	void putdown (int i)
+	{
+		state[i]=THINKING;
+		test((i+4) %5); // test L& R neighbors
+		test((i+1) %5);
+	}
+
+	void test(int i)
+	{
+		if ((state[(i+4)%5] != EATING) && (state[i] == HUNGRY) && (state[(i+1) % 5] == EATING))
+		{
+			state[i] = EATING;
+			self[i].signal();
+		}
+	}
+
+	Initialization_code()
+	{
+		for (int i = 0; i<5; i++)
+		{
+			state[i] = THINKING;
+		}
+	}
+}
+
+/* philosopher 2 wants food in thread 2 */
+DiningPhilosophers.pickup(2);
+eat
+DiningPhilosophers.putdown(2);
+
+/* philosopher 5 wants food in thread 5 */
+DiningPhilosophers.pickup(5);
+eat
+DiningPhilosophers.putdown(5);
